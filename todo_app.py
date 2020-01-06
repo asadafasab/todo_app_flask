b49cf75dd01c106c47dca73c28a894151f3626a2
6malphas <6malphas@tuta.io>
2020-01-06 13:43:05 +0100

    error page

--- todo_app.py
+++ todo_app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
@@ -9,16 +8,14 @@ db = SQLAlchemy(app)
 
 
 class ToDoList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    """Main table of db"""
+    task_id = db.Column(db.Integer, primary_key=True)
     done = db.Column(db.Boolean, default=False, nullable=False)
     title = db.Column(db.String(40), nullable=False)
     note = db.Column(db.String(250))
 
     def __repr__(self):
         return f"ToDoList('{self.title}','{self.note}')"
-
-
-db.create_all()
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -32,29 +29,29 @@ def index():
             db.session.commit()
             return redirect("/")
         except:
-            return "ADD Error..."
+            return render_template("error.html", error="ADD")
 
     else:
-        tasks = ToDoList.query.order_by(ToDoList.id).all()
+        tasks = ToDoList.query.order_by(ToDoList.task_id).all()
         return render_template("index.html", tasks=tasks)
 
 
-@app.route("/delete/<int:id>")
-def delete(id):
-    task = ToDoList.query.get_or_404(id)
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    task = ToDoList.query.get_or_404(task_id)
 
     try:
         db.session.delete(task)
         db.session.commit()
         return redirect("/")
     except:
-        return "DELETE ERROR..."
+        return render_template("error.html", error="DELETE")
     return ""
 
 
-@app.route("/update/<int:id>", methods=["POST", "GET"])
-def update(id):
-    task = ToDoList.query.get_or_404(id)
+@app.route("/update/<int:task_id>", methods=["POST", "GET"])
+def update(task_id):
+    task = ToDoList.query.get_or_404(task_id)
     if request.method == "POST":
         task.title = request.form["title"]
         task.note = request.form["note"]
@@ -62,20 +59,20 @@ def update(id):
             db.session.commit()
             return redirect("/")
         except:
-            return "UPDATE ERROR..."
+            return render_template("error.html", error="UPDATE")
     else:
         return render_template("update.html", task=task)
 
 
-@app.route("/done/<int:id>")
-def done(id):
-    task = ToDoList.query.get_or_404(id)
+@app.route("/done/<int:task_id>")
+def done(task_id):
+    task = ToDoList.query.get_or_404(task_id)
     task.done = not task.done
     try:
         db.session.commit()
         return redirect("/")
     except:
-        return "DONE ERROR..."
+        return render_template("error.html", error="DONE")
 
 
 @app.route("/about")
@@ -83,5 +80,11 @@ def about():
     return render_template("about.html")
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html', error="404 NOT FOUND"), 404
+
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
